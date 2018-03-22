82d0fa5ecf0a87b3ffbc6a95b537ac1743a288ab
Soloist <soloist1511@gmail.com>
2018-03-22 22:45:08 +0800

    a

--- url_test.py
+++ url_test.py
@@ -1,17 +1,23 @@
 from urllib import request, parse
 
-post_data = parse.urlencode([
-    ('nianji', 2016),
-    ('xuehao', ''),
-    ('mima', ''),
-    ('selec', 'http://jw.zzu.edu.cn/scripts/qscore.dll/search')
-])
 
-req = request.Request('http://jw.zzu.edu.cn/scripts/qscore.dll/search')
-req.add_header('Connection', 'keep-alive')
+def test():
+    post_data = parse.urlencode([
+        ('nianji', 2016),
+        ('xuehao', ''),
+        ('mima', ''),
+        ('selec', 'http://jw.zzu.edu.cn/scripts/qscore.dll/search')
+    ])
 
-page = request.urlopen(req, data=post_data.encode('utf-8'))
-data = page.read()
-for k, v in page.getheaders():
-    print(k, v)
-print(data.decode('gbk'))
+    req = request.Request('http://jw.zzu.edu.cn/scripts/qscore.dll/search')
+    req.add_header('Connection', 'keep-alive')
+
+    page = request.urlopen(req, data=post_data.encode('utf-8'))
+    data = page.read()
+    for k, v in page.getheaders():
+        print(k, v)
+    print(data.decode('gbk'))
+
+
+if __name__ == '__main__':
+    test()
